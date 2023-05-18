18a1ac62328bdc2e8a1dfe5dca1eca8fdf1f94b8
ninojiu <133634485+ninojiu@users.noreply.github.com>
2023-05-18 08:48:52 +0000

    Update entrypoint.sh

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -2,7 +2,7 @@
 
 # 设置各变量
 WSPATH=${WSPATH:-'argo'}
-UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
+UUID=${UUID:-'7be2c19f-db14-44d9-820e-4ccac890ea68'}
 WEB_USERNAME=${WEB_USERNAME:-'admin'}
 WEB_PASSWORD=${WEB_PASSWORD:-'password'}
 
